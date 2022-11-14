79020b9edd99609da0729b959b20bb846258a305
beardhero <damienburbine@gmail.com>
2022-11-14 14:18:02 -0700

    buy sell

--- contracts/world/CharacterManager.sol
+++ contracts/world/CharacterManager.sol
@@ -8,26 +8,36 @@ import "./base/NFTStaker.sol";
 contract CharacterManager is RegistryClient, NFTStaker{
     uint cost;
 
-    mapping(bytes32 => address) characterCreator;
-    mapping(bytes32 => address) characterPlayer;
+    mapping(bytes32 => address) public characterCreator;
+    mapping(bytes32 => address) public characterPlayer;
     mapping(address => mapping(bytes32 => uint)) public characterSeason;
-    mapping(bytes32 => uint) royalty;
-    mapping(bytes32 => address) equipped;
+    mapping(bytes32 => uint) public salePrice;
+    mapping(bytes32 => uint) public royalty;
+    mapping(bytes32 => address) public equipped;
+
 
     constructor(IRegistry registry) RegistryClient(registry) {}
 
     ///Creates character by setting season to 1
     ///@param name Names can be up to 32 characters, names are owned by addresses and transferable
     function create(bytes32 name, uint _royalty) public payable {
-        require(msg.value == cost, "Invalid payment");
+        require(msg.value == cost, "CM Invalid payment");
         characterCreator[name] = msg.sender;
         characterPlayer[name] = msg.sender;
         characterSeason[msg.sender][name] = currentWorldSeason();
         royalty[name] = _royalty;
     }
-
-    function sell() public {}
-    function buy() public {}
+    ///Updates sale price
+    ///@param price To cancel set price to 0
+    function sell(bytes32 name, uint price) public {
+        require(characterPlayer[name] == msg.sender, "CM Not your name");
+        salePrice[name] = price;
+    }
+    function buy(bytes32 name) public payable{
+        require(msg.value != 0, "CM No sale");
+        require(msg.value == salePrice[name], "CM Invalid price");
+        characterPlayer[name] = msg.sender; 
+    }
     function _equip(bytes32 tokenHash) internal {
         equipped[tokenHash] = msg.sender;
         //increase character stats
